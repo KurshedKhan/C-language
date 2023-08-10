a6a0514a66808c4fbe52fff835cd112d26834f9b
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-08-10 16:11:57 +0530

    Even number print use of array

---
+++ arrayevenint.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+ int arr[5];
+
+ for(int i = 0; i<5; i++){
+    arr[i]=i*2+2;
+ }
+ for(int i =0; i<5; i++){
+    printf("print even number using array: %d\n", arr[i]);
+ }
+
+    return 0;
+}
