6e300e0885462f88a728e6aa5efce5d81afa209f
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-08-11 15:38:09 +0530

    input / output with string

---
+++ scanfwithstring.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char str[20];
+    printf("Enter value for string:");
+    scanf("%s", str);
+
+    printf("\nValue stored in string is:%s", str);
+
+    return 0;
+}
