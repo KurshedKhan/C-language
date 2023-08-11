0f053d0c470adc550cbca00f94dc08cf44663d4c
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-08-11 15:39:23 +0530

    String introduction with length

--- string.c
+++ string.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main(){
 
-    char str[30] = "KurshedKhan";
+    char str[20] = "Nameofstring";
 
-    printf("print the string : ");
-    printf("%s\n", str);
+    printf("print the string : \n%s",str);
 
+    int length = 0;
+    
+    length = strlen(str);
+
+    printf("\nlength of string :%d",length);
 
     return 0;
+
 }
