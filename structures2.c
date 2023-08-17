9e913f3edcf825f8e649c318c6c33d566ec046d5
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-08-17 15:37:40 +0530

    Structures part 2

---
+++ structures2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student{
+
+    int rollnumber;
+    char Name[20];
+    int Class;
+    char section;
+    int age;
+    float height;
+
+};
+
+
+int main(){
+
+    struct student std1 = {12343,"Nakshatrasingh",12,'D',21,5.8};
+
+    printf("Student Details :\n name : %s \n Roll No. :%d \n Class : %d \n Section : %c \n Age : %d \n Height : %f\n",std1.Name,std1.rollnumber,std1.Class,std1.section,std1.age,std1.height);
+    
+    return 0;
+}
