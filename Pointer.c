cce307bb7c19dd7cbffb3c9361e3c6a9988a5ed5
KurshedKhan <kurshedkhanbhadra@gmail.com>
2024-07-23 10:57:17 +0530

    Pointer value and Reference

---
+++ Pointer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int num = 10;
+
+    int *num_ptr = &num;
+
+    char ch = 'a';
+
+    char *ch_ptr = &ch;
+
+    printf("Variable Value : %d\n",num);
+    printf("Address of variable : %p\n", num);
+    printf("value of pointer : %d\n",*num_ptr);
+    printf("Address of pointer : %p\n",*num_ptr);
+    printf("Address of pointer : %p\n",num_ptr);
+
+    printf("Character Pointer : %d\n ",*ch_ptr);
+
+    return 0;
+}
