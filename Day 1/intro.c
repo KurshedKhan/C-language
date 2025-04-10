f917105286e7581a5a0ece0fdc1ccfe57ae9cd6b
KurshedKhan <kurshedkhanbhadra@gmail.com>
2025-04-10 10:25:26 +0530

    C language

---
+++ Day 1/intro.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // header file // Preprocessor
+
+int main(){ // Entry Point of c program // {} ==> code block
+
+  printf("Hello , first day in c language.\n"); // printf method for display content/string
+  printf("Welcome to c lang");
+
+  return 0; // successfully run your program ==> 0
+
+} 
